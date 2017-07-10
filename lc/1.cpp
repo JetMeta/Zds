e053552060a267476021cd287943e993b929c7b4
as2120 <126.org@gmail.com>
2017-07-10 18:24:18 +0800

    d.2017-07-10.18_24_18.43

---
+++ lc/1.cpp
@@ -0,0 +1,49 @@
+struct UpperBound
+{
+	UpperBound(const int val) : target(val) {}
+	int target;
+
+	bool operator()(const int& val)
+	{
+		return val > target;
+	}
+};
+
+struct IsTarget
+{
+	IsTarget(const int val) : target(val) {}
+	int target;
+	bool operator()(const int& val)
+	{
+		return val == target;
+	}
+};
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> res;
+
+		for (auto it = nums.begin(); it != nums.end(); ++it)
+		{
+			const int right = target - *it;
+			
+			auto it_next = it + 1;
+
+			if (it_next < nums.end())
+			{
+				auto target_it = find_if(it_next, nums.end(), IsTarget(right));
+
+				if (target_it!=nums.end())
+				{
+					res.push_back(it - nums.begin());
+					res.push_back(target_it - nums.begin());
+
+					return res;
+				}
+			}
+		}
+
+		return res;
+	}
+};
---
+++ lc/7.cpp
@@ -0,0 +1,131 @@
+#include <cmath>
+#include <limits>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+	int count_bits(int x)
+	{
+		int bits_cnt = 0;
+		int y = abs(x);
+
+		while (y > 0)
+		{
+			++bits_cnt;
+
+			y >>= 1;
+		}
+
+		return bits_cnt;
+	}
+	
+	// 123 -- > 3 2 1
+	stack<int> process_rev(int x)
+	{
+		int y = abs(x);
+		stack<int> s;
+		auto b = process_seq(y);
+
+		while (!b.empty())
+		{
+			s.push(b.top());
+			b.pop();
+		}
+
+		return s;
+	}
+
+	// 123 -> 1 2 3
+	stack<int> process_seq(int x)
+	{
+		int y = abs(x);
+		stack<int> s;
+
+		while (y > 0)
+		{
+			int n = y / 10;
+			int l = y - n * 10;
+
+			s.push(l);
+			y = n;
+		}
+
+		return s;
+	}
+
+	bool compare_stack(stack<int> s1, stack<int> s2)
+	{
+		while (!s1.empty())
+		{
+			if (s2.top() > s1.top())
+			{
+				return true;
+			}
+			else
+			{
+				if (s2.top() < s1.top())
+				{
+					return false;
+				}
+			}
+
+			s1.pop();
+			s2.pop();
+		}
+
+		return false;
+	}
+
+	int result(stack<int>& s)
+	{
+		while (s.top() == 0)
+		{
+			s.pop();
+		}
+
+		int res = 0;
+		while (!s.empty())
+		{
+			res = res * 10 + s.top();
+			s.pop();
+		}
+
+		return res;
+	}
+
+	int reverse(int x) {
+		if (x == 0)
+		{
+			return 0;
+		}
+
+		if (x == std::numeric_limits<int>::min())
+		{
+			return 0;
+		}
+
+		const int sign = x > 0 ? 1 : -1;
+		// const int max_bits_cnt = count_bits(std::numeric_limits<int>::max());
+		// 2147483647
+		stack<int> s_max = process_rev(std::numeric_limits<int>::max());
+		stack<int> s2 = process_seq(x);
+
+		if (s2.size() == s_max.size())
+		{
+			if (compare_stack(s_max, s2))
+			{
+				return 0;
+			}
+			else
+			{
+				return result(s2)*sign;
+			}
+		}
+		else
+		{
+			return result(s2)*sign;
+		}
+	}
+};
