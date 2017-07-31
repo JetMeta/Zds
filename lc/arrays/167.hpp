1ec1e5ac209a85370f5d4382ceb7e5d4c026bb7d
as2120 <126.org@gmail.com>
2017-07-31 18:11:01 +0800

    d.2017-07-31.18_11_00.76

---
+++ lc/arrays/167.hpp
@@ -0,0 +1,42 @@
+﻿class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        for (auto it = numbers.cbegin(); it!=numbers.cend(); ++it)
+        {
+            const int left = target - *it;
+
+            auto cur = lower_bound(it+1, numbers.cend(), left);
+            if (cur  != numbers.cend() && *cur == left)
+            {
+                vector<int> v = { static_cast<int>(it - numbers.cbegin() + 1),static_cast<int>(cur - numbers.cbegin() + 1 )};
+                return v;
+            }
+        }
+    }
+};
+
+// final
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        int small = 0;
+        int big = numbers.size() - 1;
+        while (numbers[small] + numbers[big] != target)
+        {
+            if (numbers[small] + numbers[big]<target)
+            {
+                small++;
+            }
+            else
+            {
+                big--;
+            }
+        }
+        return vector<int>({ small + 1,big + 1 });
+
+    }
+};
---
+++ lc/arrays/476.hpp
@@ -0,0 +1,62 @@
+﻿// 3ms
+class Solution
+{
+public:
+    int find_highest_bit(int num)
+    {
+        int idx = -1;
+        int real_idx = idx;
+
+        while (num > 0 )
+        {
+            ++idx;
+
+            if (num & 1)
+            {
+                real_idx = idx;
+            }
+
+            num >>= 1;
+        }
+
+        return real_idx;
+    }
+
+    int findComplement(int num)
+    {
+        const int high = find_highest_bit(num);
+
+        int idx = 0;
+        int res = 0;
+
+        while (idx <= high)
+        {
+            if (num & 1)
+            {
+            }
+            else
+            {
+                int tmp = 1 << idx;
+                res = tmp + res;
+            }
+
+            num >>= 1;
+            ++idx;
+        }
+
+        return res;
+    }
+};
+
+// 3ms, short code, final
+class Solution {
+public:
+	int findComplement(int num) {
+		const double f2 = log10(2);
+		const double f10 = log10(num);
+		const unsigned int n = static_cast<unsigned int>(ceil(f10 / f2));
+
+		const int mask = 0xffffffff << n;
+		return ~(mask | num);
+	}
+};
