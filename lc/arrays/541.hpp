011be82bf6a940069aed48938d0df90373b62b22
as2120 <126.org@gmail.com>
2017-08-04 18:11:06 +0800

    d.2017-08-04.18_11_06.19

---
+++ lc/arrays/541.hpp
@@ -0,0 +1,47 @@
+﻿class Solution
+{
+public:
+    void reverseWord(string &s, int start, int end)
+    {
+        --end;
+        while (start < end)
+        {
+            swap(s[start++], s[end--]);
+        }
+    }
+
+    string reverseStr(string s, int k)
+    {
+        if (k < 2)
+        {
+            return s;
+        }
+
+        const size_t len = s.length();
+
+        size_t left = len;
+        size_t idx = 0;
+        size_t step = 2 * k;
+
+        while (left >= step)
+        {
+            reverseWord(s, idx, idx + k);
+            left -= step;
+            idx += step;
+        }
+
+        if (left >= k)
+        {
+            reverseWord(s,idx,idx+k);
+        }
+        else
+        {
+            if (left > 0)
+            {
+                reverseWord(s, idx, len);
+            }
+        }
+
+        return s;
+    }
+};
---
+++ lc/arrays/557.hpp
@@ -0,0 +1,79 @@
+﻿class Solution
+{
+public:
+    void reverseRange(string& s, int l, int r)
+    {
+        const int len = r - l;
+
+        if (len <= 1)
+        {
+            return;
+        }
+
+        --r;
+        while (l < r)
+        {
+            swap(s[l], s[r]);
+            ++l;
+            --r;
+        }
+    }
+
+    string reverseWords(string s)
+    {
+        const size_t len = s.length();
+
+        int l = 0, r = 0;
+        for (size_t i = 0; i < len;)
+        {
+            while (isspace(s[i]) && i < len)
+            {
+                ++i;
+            }
+
+            if (i == len)
+            {
+                return s;
+            }
+
+            l = i;
+
+            while (!isspace(s[i]) && i < len)
+            {
+                ++i;
+            }
+            r = i;
+
+            reverseRange(s, l, r);
+        }
+
+        return s;
+    }
+};
+
+class Solution
+{
+public:
+    void reverseWord(string &s, int start, int end)
+    {
+        while (start < end)
+        {
+            swap(s[start++], s[end--]);
+        }
+    }
+
+    string reverseWords(string s)
+    {
+        const int len = s.length();
+        int start = 0, end = 0;
+        while ((end = s.find(' ', start)) != -1)
+        {
+            reverseWord(s, start, end - 1);
+            start = end + 1;
+        }
+
+        reverseWord(s, start, len - 1);
+
+        return s;
+    }
+};
