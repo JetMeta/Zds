6f221d0759c379715296d6f3332c20de50455db3
JetMeta <126.org@gmail.com>
2017-08-06 00:06:26 +0800

    d.2017/08/06.00_06_26.56

---
+++ lc/arrays/575.hpp
@@ -0,0 +1,26 @@
+﻿class Solution
+{
+public:
+    int distributeCandies(vector<int>& candies)
+    {
+        int cnt = 0;
+		bitset<200001> b;
+        const int max_candies = candies.size() / 2;
+
+        for (auto it = candies.cbegin(); it != candies.cend(); ++it)
+        {
+            if (!b[*it + 100000])
+            {
+                b[*it + 100000] = true;
+                ++cnt;
+            }
+
+            if (cnt >= max_candies)
+            {
+                return max_candies;
+            }
+        }
+
+        return cnt;
+    }
+};
