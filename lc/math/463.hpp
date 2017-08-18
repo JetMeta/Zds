ec536929a5e1ba7188e19a170c34e51aa0578292
as2120 <126.org@gmail.com>
2017-08-18 18:09:33 +0800

    d.2017-08-18.18_09_32.94

---
+++ lc/math/463.hpp
@@ -0,0 +1,52 @@
+﻿class Solution
+{
+public:
+    int girdVal(const size_t i, const size_t j, vector<vector<int>>& grid, const size_t w, const size_t h)
+    {
+        if (i >= 0 && i < h && j >= 0 && j < w)
+        {
+            return grid[i][j];
+        }
+
+        return 0;
+    }
+
+    int calc(const size_t i, const size_t j, vector<vector<int>>& grid,const size_t w,const size_t h)
+    {
+        const int size_cnt =
+            !girdVal(i - 1, j, grid, w, h) +
+            !girdVal(i + 1, j, grid, w, h) +
+            !girdVal(i, j - 1, grid, w, h) +
+            !girdVal(i, j + 1, grid, w, h);
+        return size_cnt;
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid)
+    {
+        if (grid.size() == 0)
+        {
+            return 0;
+        }
+
+        if (grid[0].size() == 0)
+        {
+            return 0;
+        }
+
+        const size_t h = grid.size(), w = grid[0].size();
+
+        int perimeter = 0;
+        for (size_t i = 0; i < h; ++i)
+        {
+            for (size_t j = 0; j < w; ++j)
+            {
+                if (grid[i][j])
+                {
+                    perimeter += calc(i, j, grid, w, h);
+                }
+            }
+        }
+
+        return perimeter;
+    }
+};
---
+++ lc/string/647.hpp
@@ -0,0 +1,52 @@
+﻿class Solution
+{
+public:
+    int odd_cnt(const string& s,size_t idx,const size_t len)
+    {
+        int cnt = 0;
+        for(size_t i = 0 ; ((idx+i) <len)&&((idx-i) >= 0) ; ++i)
+        {
+            if(s[idx + i] == s[idx -i])
+            {
+                ++cnt;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return cnt;
+    }
+
+    int even_cnt(const string& s,size_t idx,const size_t len)
+    {
+        int cnt = 0;
+        for(size_t i = 0 ; ((idx+i+1) <len)&&((idx-i) >= 0) ; ++i)
+        {
+            if(s[idx-i] == s[idx+i+1])
+            {
+                ++cnt;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return cnt;
+    }
+
+    int countSubstrings(string s)
+    {
+        const size_t len = s.length();
+        int cnt = 0;
+
+        for (size_t i = 0; i < len; ++i)
+        {
+            cnt += odd_cnt(s,i,len);
+            cnt += even_cnt(s,i,len);
+        }
+
+        return cnt;
+    }
+};
