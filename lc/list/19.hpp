f9a21418b90e931a7f22a1dbe8ad96eb5e03aa5c
as2120 <126.org@gmail.com>
2017-08-25 18:08:11 +0800

    d.2017-08-25.18_08_10.85

---
+++ lc/list/19.hpp
@@ -0,0 +1,37 @@
+﻿class Solution
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
+        int idx = 0;
+
+        ListNode* cur = head;
+        ListNode* del = head;
+        ListNode* prev = head;
+
+        while (cur)
+        {
+            if (idx > n - 1)
+            {
+                prev = del;
+                del = del->next;
+            }
+
+            ++idx;
+
+            cur = cur->next;
+        }
+
+        if (del != head)
+        {
+            prev->next = del->next;
+
+            return head;
+        }
+        else
+        {
+            head = head->next;
+            return head;
+        }
+    }
+};
---
+++ lc/math/9.hpp
@@ -0,0 +1,48 @@
+﻿class Solution
+{
+public:
+    int cnt_digits(int x)
+    {
+        x = abs(x);
+        return (x < 10 ? 1 :
+                (x < 100 ? 2 :
+                 (x < 1000 ? 3 :
+                  (x < 10000 ? 4 :
+                   (x < 100000 ? 5 :
+                    (x < 1000000 ? 6 :
+                     (x < 10000000 ? 7 :
+                      (x < 100000000 ? 8 :
+                       (x < 1000000000 ? 9 :
+                        10)))))))));
+    }
+
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        const int len = cnt_digits(x);
+
+        int val = abs(x);
+        int tens = pow(10, len-1);
+
+        while(val)
+        {
+            int s = val / tens;
+            int t = val % 10;
+
+            if (s != t)
+            {
+                return false;
+            }
+
+            val -= s*tens;
+            val = val / 10;
+            tens /= 100;
+        }
+
+        return true;
+    }
+};
