091b6774bcb76e58119b670a4e7869c8db437184
JetMeta <126.org@gmail.com>
2017-08-27 23:36:24 +0800

    d.2017/08/27.23_36_23.50

---
+++ lc/arrays/16.hpp
@@ -0,0 +1,44 @@
+﻿class Solution
+{
+public:
+    int threeSumClosest(vector<int>& nums, int target)
+    {
+        sort(nums.begin(), nums.end());
+
+        const size_t len = nums.size();
+        int sum = std::numeric_limits<int>::max();
+        int ans = 0;
+        for (size_t i = 0; i < len - 2; ++i)
+        {
+            size_t l = i + 1;
+            size_t r = len - 1;
+
+            while (l < r)
+            {
+                const int ans_v = nums[i] + nums[l] + nums[r];
+                const int left = abs(ans_v - target);
+                if (left == 0)
+                {
+                    return target;
+                }
+
+                if (nums[i] + nums[l] + nums[r] < target)
+                {
+                    ++l;
+                }
+                else
+                {
+                    --r;
+                }
+
+                if (left < sum)
+                {
+                    sum = left;
+                    ans = ans_v;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
---
+++ lc/list/2.hpp
@@ -0,0 +1,83 @@
+﻿class Solution
+{
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    {
+        ListNode* sum = nullptr;
+        ListNode* head = nullptr;
+
+        ListNode* cur1 = l1;
+        ListNode* cur2 = l2;
+
+        int carrier = 0;
+        int left = 0;
+        while (cur1 && cur2)
+        {
+            int val = cur1->val + cur2->val + carrier;
+
+            if (val > 9)
+            {
+                left = val - 10;
+                carrier = 1;
+            }
+            else
+            {
+                carrier = 0;
+                left = val;
+            }
+
+            if (!head)
+            {
+                head = new ListNode(left);
+                sum = head;
+            }
+            else
+            {
+                sum->next = new ListNode(left);
+                sum = sum->next;
+            }
+
+            cur1 = cur1->next;
+            cur2 = cur2->next;
+        }
+
+        ListNode* left_list = cur1 ? cur1 : cur2;
+
+        while (left_list)
+        {
+            int val = left_list->val + carrier;
+
+            if (val > 9)
+            {
+                left = val - 10;
+                carrier = 1;
+            }
+            else
+            {
+                carrier = 0;
+                left = val;
+            }
+
+            if (!head)
+            {
+                head = new ListNode(left);
+                sum = head;
+            }
+            else
+            {
+                sum->next = new ListNode(left);
+                sum = sum->next;
+            }
+
+            left_list = left_list->next;
+        }
+
+        if (carrier)
+        {
+            sum->next = new ListNode(carrier);
+            sum = sum->next;
+        }
+
+        return head;
+    }
+};
