e5fd075a8090b9e40780e5736eff56f2a1f28dc5
as2120 <126.org@gmail.com>
2017-08-29 18:05:27 +0800

    d.2017-08-29.18_05_27.22

---
+++ lc/dfs/513.hpp
@@ -0,0 +1,39 @@
+﻿class Solution
+{
+public:
+    void f(TreeNode* node, int level, int& max_level, int& left_most_val)
+    {
+        if (node)
+        {
+            if (!node->left && !node->right)
+            {
+                if (level > max_level)
+                {
+                    max_level = level;
+                    left_most_val = node->val;
+                }
+
+                return;
+            }
+        }
+
+        if (node->left)
+        {
+            f(node->left, level+1, max_level, left_most_val);
+        }
+
+        if (node->right)
+        {
+            f(node->right, level+1, max_level, left_most_val);
+        }
+    }
+
+    int findBottomLeftValue(TreeNode* root)
+    {
+        int max_level = 0;
+        int left_most_val = root->val;
+        f(root, 0, max_level, left_most_val);
+
+        return left_most_val;
+    }
+};
