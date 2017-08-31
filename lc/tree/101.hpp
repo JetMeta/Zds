9eb9ee6f1ed97c20559a96a055aa200692ba5915
as2120 <126.org@gmail.com>
2017-08-31 18:19:02 +0800

    d.2017-08-31.18_19_02.09

---
+++ lc/tree/101.hpp
@@ -0,0 +1,40 @@
+﻿class Solution
+{
+public:
+    bool isMirror(TreeNode* l, TreeNode* r)
+    {
+        if (l == NULL && r == NULL)
+        {
+            return true;
+        }
+
+        if ((!l && r) || (l && !r))
+        {
+            return false;
+        }
+
+        if (l->val != r->val)
+        {
+            return false;
+        }
+
+        bool res = isMirror(l->left, r->right);
+
+        if (res)
+        {
+            res = isMirror(l->right, r->left);
+        }
+
+        return res;
+    }
+
+    bool isSymmetric(TreeNode* root)
+    {
+        if (!root)
+        {
+            return true;
+        }
+
+        return isMirror(root->left, root->right);
+    }
+};
