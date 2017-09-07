3c3f3be21c7ae71a03de7c773ee9780b8fa67c92
JetMeta <126.org@gmail.com>
2017-09-07 08:44:26 +0800

    d.2017/09/07.08_44_25.91

---
+++ lc/arrays/283.hpp
@@ -0,0 +1,23 @@
+﻿class Solution
+{
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        const size_t length = nums.size();
+        size_t last_non_zero = 0;
+
+        for (size_t i = 0; i < length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[last_non_zero] = nums[i];
+                ++last_non_zero;
+            }
+        }
+
+        for (size_t i = last_non_zero; i < length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+};
