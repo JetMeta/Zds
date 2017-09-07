38af27ab1189e0a54fe2c8ae14c9fbd49395f0fd
as2120 <126.org@gmail.com>
2017-09-07 09:38:52 +0800

    d.2017-09-07.09_38_52.27

---
+++ lc/stack/94.hpp
@@ -0,0 +1,29 @@
+﻿class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        stack<TreeNode*> s;
+        vector<int> v;
+
+        TreeNode* cur = root;
+
+        while (!s.empty() || cur)
+        {
+            if (cur)
+            {
+                s.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                cur = s.top();
+                s.pop();
+                v.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+
+        return v;
+    }
+};
---
+++ lc/tree/98.hpp
@@ -0,0 +1,43 @@
+﻿class Solution
+{
+public:
+	void inorder2(TreeNode* root,int* last,bool & init,bool& res)
+	{
+		if (root)
+		{
+			inorder2(root->left,last, init,res);
+
+			{
+				if (!init)
+				{
+					init = true;
+					*last = root->val;
+				}
+				else
+				{
+					if (root->val <= *last)
+					{
+						res = false;
+						return;
+					}
+					else
+					{
+						*last = root->val;
+					}
+				}
+			}
+
+			inorder2(root->right, last,init,res);
+		}
+	}
+
+    bool isValidBST(TreeNode* root)
+    {
+		int v = 0;
+		bool res = true;
+		bool init = false;
+		inorder2(root, &v,init, res);
+
+		return res;
+    }
+};
