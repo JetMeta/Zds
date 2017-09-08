9b488a0e7e2ae31239079be4681ab22d1b4869af
JetMeta <126.org@gmail.com>
2017-09-08 08:09:50 +0800

    d.2017/09/08.08_09_50.88

---
+++ lc/string/520.hpp
@@ -0,0 +1,56 @@
+﻿class Solution
+{
+public:
+    bool detectCapitalUse(string word)
+    {
+        size_t length = word.length();
+
+        if (length > 1)
+        {
+            if (isupper(word[0]))
+            {
+                if (isupper(word[1]))
+                {
+                    for (size_t i = 2; i < length; i++)
+                    {
+                        if (islower(word[i]))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    if (islower(word[1]))
+                    {
+                        for (size_t i = 2; i < length; i++)
+                        {
+                            if (isupper(word[i]))
+                            {
+                                return false;
+                            }
+                        }
+
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                for (size_t i = 1; i < length; i++)
+                {
+                    if (isupper(word[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        return true;
+    }
+};
---
+++ lc/string/539.hpp
@@ -0,0 +1,60 @@
+﻿class Solution
+{
+public:
+    int strToTime(const string& str)
+    {
+        string::size_type sz;
+
+        const int nh = stoi(str, &sz);
+        const int nm = stoi(&str[sz + 1], &sz);
+
+        return nh * 60 + nm;
+    }
+
+    int findMinDifference(vector<string>& timePoints)
+    {
+        size_t length = timePoints.size();
+        const int day = 24 * 60;
+        int res = day;
+        vector<int> v(length);
+
+        for (size_t i = 0; i < length; i++)
+        {
+            const int cur = strToTime(timePoints[i]);
+
+            v[i] = cur;
+        }
+
+        for (size_t i = 0; i < length; i++)
+        {
+            for (size_t j = i + 1; j < length; ++j)
+            {
+                int r1 = 0;
+                int r2 = 0;
+                if (v[i] == v[j])
+                {
+                    return 0;
+                }
+                else
+                {
+                    if (v[i] > v[j])
+                    {
+                        r1 = v[i] - v[j];
+                        r2 = v[j] + day - v[i];
+
+                        res = min(res, min(r1, r2));
+                    }
+                    else
+                    {
+                        r1 = v[j] - v[i];
+                        r2 = v[i] + day - v[j];
+
+                        res = min(res, min(r1, r2));
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+};
