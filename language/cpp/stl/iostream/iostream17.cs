b8858783fff1d1022a3c8163388ed239457c05ed
as2120 <126.org@gmail.com>
2017-11-02 17:47:14 +0800

    d.2017-11-02.17_47_13.68

---
+++ language/cpp/stl/iostream/iostream17.cs
@@ -0,0 +1,113 @@
+﻿/* #FH #CC //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K.F593184509 +----+----+----+----+----+
++  0.FileInfo: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+        File : E:\MyDoc\GitHub\Zds\language\cpp\stl\iostream\iostream17.cs.F593184509 ----+----+
+       Motto : No one can make you feel inferior without your consent.+----+----+----+----+----+
+       Motto : The death of fear is in doing what you fear to do.+----+----+----+----+----+----+
+       Motto : It's hard to beat a person who never gives up.----+----+----+----+----+----+----+
+       Motto : Take what you do seriously. Not yourself.----+----+----+----+----+----+----+----+
+       Motto : Life begins where your comfort zone ends.----+----+----+----+----+----+----+----+
+       Motto : To know and not do, is not yet to know.-+----+----+----+----+----+----+----+----+
+       Motto : The more you do , the more you can do.--+----+----+----+----+----+----+----+----+
+       Motto : Do what you do best, better.--+----+----+----+----+----+----+----+----+----+----+
+       Motto : Get Your Hands Dirty.----+----+----+----+----+----+----+----+----+----+----+----+
+      Author : CopyRight (C) 2017 126.org@gmail.com (K->30,T5,M&M,W)--+----+----+----+----+----+
+
++  1.ID & Desc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  2.Req & Repro: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  3.Folder & File: +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  4.Cause & Solution: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  5.Todo & Done: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  6.Commit: --+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  7.Issue: ---+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  8.Summary: -+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
++  9.Misc: ----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
+***********************************************************************************************/
+
+#region 11-02 R#.0    @Ver.Info
+       Zey_S : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+		R#
+       Zey_E : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+       Zcl_S : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+       Zcl_E : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+      ZdoIdx : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+      KmtIdx : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     TimeCnt : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     Reg.Cnt : //z 17-11-02 13:32:43 L.59 '37637 T892739564 .K ~1   --+----+----+----+----+----+
+     File.Op : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     Version : //z 17-11-02 13:40:25 L.59 '37175 T893853547 .K ~20      R+.1    L+.92   --+----+
+     #1   V+ : //z 17-11-02 13:40:17 L.59 '37183 T893853516 .K ~19      R+.1    L+.93   
+     #0   R+ : //z 17-11-02 13:32:43 L.59 '37637 T892739564 .K ~3       R+.1    L+.64   V+.3    
+     Zndex_S : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     Zndex_E : //z 17-11-02 13:40:25 L.59 '37175 T893853547 .K ~19  --+----+----+----+----+----+
+     K   Ter : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K  Mers : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K Kersi : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     KVersio : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K Versi : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K  Vers : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K   Ver : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K    Ve : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     K     V : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+     KaizenZ : //z 17-11-02 13:31:08 L.59 '37732 T892703500 .K ~0   --+----+----+----+----+----+
+#endregion #HE //z 2017-11-02 13:31:08 L.59 '37732 BG57IV3 T892703500 .K.F593184509 -+----+----+
+
+#region 11-02 R#.1    @'\n' vs endl
+01. 
+The varying line-ending characters don't matter, assuming the file is open in text mode, which is what you get unless you ask for binary. The compiled program will write out the correct thing for the system compiled for.
+
+The only difference is that std::endl flushes the output buffer, and '\n' doesn't. If you don't want the buffer flushed frequently, use '\n'. If you do (for example, if you want to get all the output, and the program is unstable), use std::endl.
+---
+也有另一种说法
+"In many implementations, standard output is line-buffered, and writing '\n' causes a flush anyway, unless std::cout.sync_with_stdio(false) was executed." 
+---
+The difference can be illustrated by the following:
+
+std::cout << std::endl;
+is equivalent to
+
+std::cout << '\n' << std::flush;
+So,
+
+Use std::endl If you want to force an immediate flush to the output.
+Use \n if you are worried about performance (which is probably not the case if you are using the << operator).
+I use \n on most lines.
+Then use std::endl at the end of a paragraph (but that is just a habit and not usually necessary).
+
+Contrary to other claims, the \n character is mapped to the correct platform end of line sequence only if the stream is going to a file (std::cin and std::cout being special but still files (or file-like)).
+---
+Why I Prefer ‘\n’ To std::endl
+Both serve the same purpose, putting a new line. The only difference is that endl causes flushing of the output buffer everytime it is called where as ‘\n’ does not.
+
+So, if you are writing al the alphabets of English to a file using the following code,
+
+#include <iostream>;
+#include <fstream>;
+
+using namespace std;
+
+int main()
+{
+    ofstream MyFileStream(“Alphabets.txt”, ios:: out);
+    for (char Index = ‘A’; Index <= 'Z'; ++Index)
+    {
+        MyFileStream << Index << endl;
+    }
+    return 0;
+}
+
+the output buffer will be flushed 26 times while the same code with ‘\n’ instead of endl will cause it to be flushed only once and that is when the program exits (assuming that the output buffer is larger than 26 bytes).
+
+Some Background Info
+
+Anything to be output is first queued into an output buffer and written to the device (hard disk, monitor etc…) when the queue is full. Of course, this is to ensure speed of execution as the access to the external device is less frequent.
+If you are in a situation where you have to avoid buffering, you can use std::endl instead of ‘\n’. An example of the above situation is when you are using old style debugging using multiple cout statements between code lines  to see where exactly a crash occurs.
+#endregion //z 2017-11-02 13:32:43 L.59 '37637 BG57IV3 T892739564 .K.F593184509 +----+----+----+
