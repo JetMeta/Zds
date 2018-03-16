de61251707c11b33a2016d62b5fec004516fb719
JetMeta <126.org@gmail.com>
2018-03-17 07:35:51 +0800

    d.2018/03/17.07_35_50.95

---
+++ lc/bsearch/153.hpp
@@ -0,0 +1,34 @@
+﻿class Solution
+{
+public:
+    int findMin(vector<int>& nums, const int l, const int r)
+    {
+        if (l >= r)
+        {
+            return nums[r];
+        }
+
+        if (nums[l] <= nums[r])
+        {
+            return nums[l];
+        }
+        else
+        {
+            int m = (l + r) >> 1;
+
+            if (nums[m] >= nums[l])
+            {
+                return findMin(nums, m + 1, r);
+            }
+            else
+            {
+                return findMin(nums, l, m);
+            }
+        }
+    }
+
+    int findMin(vector<int>& nums)
+    {
+        return findMin(nums, 0, nums.size() - 1);
+    }
+};
